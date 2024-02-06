1f3d3e65eeca98a4199a344c66842e624b5564d2
IkramBOUHALA <benibrahimikram3@gmail.com>
2024-02-06 17:19:53 +0100

    add an update version

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,46 +1,87 @@
 const { StatusCodes } = require("http-status-codes");
-const Product = require("../model/productlist");
+// const Product = require("../model/productlist");
 const productlist = require("../model/productlist");
+const customError = require('../error/error')
+
 
 const createproductcontroller = async (req, res) => {
-  try {
-    const { name, price, series_number } = req.body;
+  const product = await productlist.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ product });
+}
+// const createproductcontroller = async (req, res) => {
+//   // try {
+//     const { name, price, series_number } = req.body;
 
-    // Validate required fields
-    if (!name) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "Name is required" });
-    }
+//     // Validate required fields
+//     if (!name) {
+//       return res
+//         .status(StatusCodes.BAD_REQUEST)
+//         .json({ error: "Name is required" });
+//     }
 
-    // Validate data using Mongoose schema
-    const product = new Product({
-      name,
-      price,
-      series_number,
-    });
+//     // Validate data using Mongoose schema
+//     const product = new Product({
+//       name,
+//       price,
+//       series_number,
+//     });
 
-    // Save the product
-    await product.save();
+//     // Save the product
+//     await product.save();
 
-    res.status(StatusCodes.CREATED).json({ product });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: "Internal Server Error" });
-  }
-};
+//     res.status(StatusCodes.CREATED).json({ product });
+//   // } catch (error) {
+//   //   console.error(error);
+//   //   res
+//   //     .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//   //     .json({ error: "Internal Server Error" });
+//   // }
+// };
 
 const getAllProductsController = async (req, res) => {
-  try {
+  // try {
     const products = await productlist.find();
     res.status(StatusCodes.OK).json({ products });
-  } catch (error) {
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: "internal server error" });
-  }
+  // } catch (error) {
+  //   res
+  //     .status(StatusCodes.INTERNAL_SERVER_ERROR)
+  //     .json({ error: "internal server error" });
+  // }
 };
 
-module.exports = { createproductcontroller, getAllProductsController };
+const getProductById=async(req,res)=>{
+  const{id:productId}=req.params;
+  const product=await productlist.findOne({_id:productId})
+  if (!product) throw new customError.NotFoundError(`no product with id: ${id}`) 
+  res.status(StatusCodes.OK)
+  .json({product})
+}
+
+const deleteProduct=async(req,res)=>{
+  const{id:productId}=req.params;
+  const product= await productlist.findOne({_id:productId})
+  if (product) {
+    await productlist.findOneAndDelete({_id:productId})
+    res.status(StatusCodes.OK)
+    .json({message: "product removed"})
+  }else{
+    res.status(StatusCodes.NOT_FOUND)
+    .json({message: "product not found"})
+  }
+}
+
+const updateProduct= async(req,res)=>{
+  const{id:productId}=req.params;
+  const product= await productlist.findOne({_id:productId})
+  if (product) {
+    await productlist.findOneAndUpdate({_id:productId},req.body)
+    res.status(StatusCodes.OK)
+    .json({message: "product updated"})
+  }else{
+    res.status(StatusCodes.NOT_FOUND)
+    .json({message: "product not found"})
+  }
+  
+}
+
+module.exports = { createproductcontroller, getAllProductsController, getProductById, deleteProduct,updateProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const {
   createproductcontroller,
   getAllProductsController,
+  getProductById,
+  deleteProduct,
+  updateProduct
 } = require("../controllers/productControllers");
 
-router.route("/").post(createproductcontroller)
-.get(getAllProductsController);
+router.route("/").post(createproductcontroller).get(getAllProductsController);
+router.route("/:id").get(getProductById).delete(deleteProduct).patch(updateProduct);
 
 module.exports = router;
